75f181ec767e56e3ef1727a73adcb5e1a2b482e1
Jing Zhou <JZhou3083@gmail.com>
2025-05-24 14:35:22 +0100

    Create the project

--- autotrader_scraper.py
+++ autotrader_scraper.py
@@ -46,7 +46,7 @@ cars = [
 def scrape_autotrader(cars, criteria):
     chrome_options = Options()
     chrome_options.add_argument("_tt_enable_cookie=1")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
     driver = webdriver.Chrome(options = chrome_options)
     data = []
 
